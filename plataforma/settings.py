2161f5a2a864d8ba17c29eeb77178a6dfe1913fc
Adrian Vargas <adrianfelipevr@gmail.com>
2026-05-31 19:56:26 -0500

    Configurar PostgreSQL

--- plataforma/settings.py
+++ plataforma/settings.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import dj_database_url
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -51,11 +53,17 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'plataforma.wsgi.application'
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
